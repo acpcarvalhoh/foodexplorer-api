a7d04dbadf35eac3353348196812e62c5c68e90a
acp <carvalhohuzumak@gmail.com>
2023-12-08 16:28:32 -0300

    finalizado projeto

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ database();
 
 app.use("/files", express.static(uploadsconfig.UPLOADS_FOLDER));
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173/"],
+    origin: ["https://legendary-shortbread-6d0185.netlify.app/", "http://127.0.0.1:5173/"],
     credentials: true,
 }));
 app.use(routes);
